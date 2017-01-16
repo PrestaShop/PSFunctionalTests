2a5b37df4026f451f6c378d0cea35e2c619efbad
nesrine abdmouleh <nesrine.abdmouleh@sifast.com>
2017-01-16 15:39:15 +0100

    add

--- test/itg/1.7/scenario/FO/buy_product.webdriverio.js
+++ test/itg/1.7/scenario/FO/buy_product.webdriverio.js
@@ -77,8 +77,8 @@ describe('buy_product', function(){
 				.click(this.selector.command_button_checkout)
 				.waitForExist(this.selector.checkout_step2_continue_button, 90000)
 				.click(this.selector.checkout_step2_continue_button)
-				.waitForExist(this.selector.checkout_step3_continue_button, 90000)
-				.click(this.selector.checkout_step3_continue_button)
+				//.waitForExist(this.selector.checkout_step3_continue_button, 90000)
+				//.click(this.selector.checkout_step3_continue_button)
 				.waitForExist(this.selector.checkout_step4_payment, 90000)
 				.getText(this.selector.checkout_total).then(function(text) {
 					var checkout_total = text;
