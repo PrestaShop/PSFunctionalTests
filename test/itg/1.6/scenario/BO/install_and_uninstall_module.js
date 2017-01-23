c7ed353c5c4d3aac462d8bc111d47d0856a71a78
Basma YANGUI <basma.yangui@prestashop.com>
2017-01-23 10:07:58 +0100

    Making comprehensible messages (2)

--- test/itg/1.6/scenario/BO/install_and_uninstall_module.js
+++ test/itg/1.6/scenario/BO/install_and_uninstall_module.js
@@ -3,7 +3,7 @@ var should = require('should');
 var common = require('../../common.webdriverio');
 var globals = require('../../globals.webdriverio.js');
 
-describe('install_and_uninstall_module', function(){
+describe('The Install of a Module and its Uninstall', function(){
 	common.initMocha.call(this);
 	
 	before(function(done){
@@ -13,60 +13,68 @@ describe('install_and_uninstall_module', function(){
 	after(common.after);
 
 	
-		it('loggin BO', function(done){
-			this.client
-				.signinBO()
-				.call(done);
-		});	
+    describe('Log in in Back Office', function(done){
+        it('should log in successfully in BO', function(done){
+            this.client
+                .signinBO()
+                .call(done);
+        });
+    });
 
-		
-		it('go_to_module', function(done){
-			this.client
-				.waitForExist(this.selector.menu, 60000)
-				.click(this.selector.modules_menu)
-				.waitForExist(this.selector.modules_search, 60000)
-				.call(done);
-		});
-		
-		it('install_module', function(done){			
-				this.client
-				/*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
-					should(present).be.equal(false);
-				})*/
-				.setValue(this.selector.modules_search, module_tech_name)
-				.waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
-				.click('//i[@class="icon-plus-sign-alt" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
-				.waitForExist('//div[@class="alert alert-success"]', 60000)
-				.call(done);
-		});
-		
-		it('go_to_module', function(done){
-			this.client
-				.waitForExist(this.selector.menu, 60000)
-				.click(this.selector.modules_menu)
-				.waitForExist(this.selector.modules_search, 60000)
-				.call(done);
-		});
-		
-		it('uninstall_module', function(done){			
-				this.client
-				/*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
-					should(present).be.equal(false);
-				})*/
-				.setValue(this.selector.modules_search, module_tech_name)
-				.waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
-				.click('//button[@class="btn btn-default dropdown-toggle" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
-				.waitForExist('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]', 60000)
-				.click('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]')
-				.alertAccept()
-				.waitForExist('//div[@class="alert alert-success"]', 60000)
-				.call(done);
-		});
-		
-		it('logout_BO', function(done){
-			this.client
-				.signoutBO()
-				.call(done);
-		});
+
+    describe('Install module', function(done){
+        it('sould go to the module', function(done){
+            this.client
+                .waitForExist(this.selector.menu, 60000)
+                .click(this.selector.modules_menu)
+                .waitForExist(this.selector.modules_search, 60000)
+                .call(done);
+        });
+
+        it('should install the module', function(done){
+                this.client
+                /*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
+                    should(present).be.equal(false);
+                })*/
+                .setValue(this.selector.modules_search, module_tech_name)
+                .waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
+                .click('//i[@class="icon-plus-sign-alt" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
+                .waitForExist('//div[@class="alert alert-success"]', 60000)
+                .call(done);
+        });
+    });
+
+    describe('Uninstall module', function(done){
+        it('should go to the module', function(done){
+            this.client
+                .waitForExist(this.selector.menu, 60000)
+                .click(this.selector.modules_menu)
+                .waitForExist(this.selector.modules_search, 60000)
+                .call(done);
+        });
+
+        it('should uninstall_module', function(done){
+                this.client
+                /*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
+                    should(present).be.equal(false);
+                })*/
+                .setValue(this.selector.modules_search, module_tech_name)
+                .waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
+                .click('//button[@class="btn btn-default dropdown-toggle" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
+                .waitForExist('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]', 60000)
+                .click('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]')
+                .alertAccept()
+                .waitForExist('//div[@class="alert alert-success"]', 60000)
+                .call(done);
+        });
+    });
+
+    describe('Log out in Back Office', function(done){
+        it('should log out successfully in BO', function(done){
+            this.client
+                .signoutBO()
+                .call(done);
+        });
+    });
 
 });	
--- test/itg/1.6/scenario/BO/uninstall_module.js
+++ test/itg/1.6/scenario/BO/uninstall_module.js
@@ -3,7 +3,7 @@ var should = require('should');
 var common = require('../../common.webdriverio');
 var globals = require('../../globals.webdriverio.js');
 
-describe('uninstall_module', function(){
+describe('The Uninstall of a Module', function(){
 	common.initMocha.call(this);
 	
 	before(function(done){
@@ -12,43 +12,45 @@ describe('uninstall_module', function(){
 	});
 	after(common.after);
 
-	
-		it('loggin BO', function(done){
-			this.client
-				.signinBO()
-				.pause(60000);
-			this.client
-				.call(done);
-		});
+	describe('Log in in Back Office', function(done){
+        it('should log in successfully in BO', function(done){
+            this.client
+                .signinBO()
+                .call(done);
+        });
+    });
 
-		
-		it('go_to_module', function(done){
-			this.client
-				.waitForExist(this.selector.menu, 60000)
-				.click(this.selector.modules_menu)
-				.waitForExist(this.selector.modules_search, 60000)
-				.call(done);
-		});
-		
-		it('uninstall_module', function(done){			
-				this.client
-				/*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
-					should(present).be.equal(false);
-				})*/
-				.setValue(this.selector.modules_search, module_tech_name)
-				.waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
-				.click('//button[@class="btn btn-default dropdown-toggle" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
-				.waitForExist('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]', 60000)
-				.click('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]')
-				.alertAccept()
-				.waitForExist('//div[@class="alert alert-success"]', 60000)
-				.call(done);
-		});
-		
-		it('logout_BO', function(done){
-			this.client
-				.signoutBO()
-				.call(done);
-		});
+	describe('Uninstall module', function(done){
+        it('should go to the module', function(done){
+            this.client
+                .waitForExist(this.selector.menu, 60000)
+                .click(this.selector.modules_menu)
+                .waitForExist(this.selector.modules_search, 60000)
+                .call(done);
+        });
+
+        it('should uninstall_module', function(done){
+                this.client
+                /*.isExisting("//*[@class=\"alert alert-danger\"]").then(function(present) {
+                    should(present).be.equal(false);
+                })*/
+                .setValue(this.selector.modules_search, module_tech_name)
+                .waitForExist('//table[@id="module-list"]/tbody/tr[not(@style)]//span[text()="' + module_tech_name+ '"]', 60000)
+                .click('//button[@class="btn btn-default dropdown-toggle" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]')
+                .waitForExist('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]', 60000)
+                .click('//ul[@class="dropdown-menu" and ancestor::tr[not(@style)]//span[text()="' + module_tech_name+ '"]]/li/a[@title="Uninstall"]')
+                .alertAccept()
+                .waitForExist('//div[@class="alert alert-success"]', 60000)
+                .call(done);
+        });
+    });
+
+    describe('Log out in Back Office', function(done){
+        it('should log out successfully in BO', function(done){
+            this.client
+                .signoutBO()
+                .call(done);
+        });
+    });
 
 });	
