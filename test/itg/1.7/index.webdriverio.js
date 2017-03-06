ef70bd323b46eb6ae095a8f458788be27dbefe82
Basma YANGUI <basma.yangui@prestashop.com>
2017-03-06 10:59:50 +0100

    Example of a good module

--- test/itg/1.7/index.webdriverio.js
+++ test/itg/1.7/index.webdriverio.js
@@ -28,7 +28,7 @@ describe('Allscenario', function(){
 
 
 	//create a product in BO and check it in FO
-	/*require('./scenario/BO/create_product.webdriverio');
+	require('./scenario/BO/create_product.webdriverio');
 	require('./scenario/FO/check_product.webdriverio');
 	
 	//create an order in FO and check it in BO
@@ -36,7 +36,7 @@ describe('Allscenario', function(){
 	require('./scenario/BO/check_order.webdriverio');
 	
 	//create an account in FO
-	require('./scenario/FO/create_account.webdriverio');*/
+	require('./scenario/FO/create_account.webdriverio');
 	
 	if (typeof module_tech_name !== 'undefined' && module_tech_name != "None"){
 		require('./scenario/BO/uninstall_module.js');
