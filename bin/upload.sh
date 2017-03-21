14112836a01da064aaf46625994121cb9bdf0920
Basma YANGUI <basma.yangui@prestashop.com>
2017-03-21 11:09:14 +0100

    Do not upload screenshots if folder is empty

--- bin/upload.sh
+++ bin/upload.sh
@@ -2,9 +2,16 @@
 
 FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
 ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.tar.bz2
-echo "Creating archive $ARCHIVE"
-tar -cjf $ARCHIVE -C test/itg/$PS_VERSION/screenshots .
-FILESIZE=$(stat -c%s "$ARCHIVE")
-echo "Finished archive (size $FILESIZE), starting Google Drive upload"
-./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
-echo "Finished Google Drive upload"
+DIR="test/itg/$PS_VERSION/screenshots"
+
+# look for empty dir
+if [ "$(ls -A $DIR)" ]; then
+    echo "Creating archive $ARCHIVE"
+    tar -cjf $ARCHIVE -C $DIR .
+    FILESIZE=$(stat -c%s "$ARCHIVE")
+    echo "Finished archive (size $FILESIZE), starting Google Drive upload"
+    ./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
+    echo "Finished Google Drive upload"
+else
+    echo "$DIR is empty"
+fi
