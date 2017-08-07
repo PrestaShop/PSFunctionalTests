5f6c00dd73527970f48b706df65db72fded85300
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-07 16:42:22 +0100

    Delete windowHandleMaximize

--- test/itg/1.6/common.webdriverio.js
+++ test/itg/1.6/common.webdriverio.js
@@ -118,12 +118,10 @@ module.exports = {
                 client = webdriverio
                     .remote(options2)
                     .init()
-                    .windowHandleMaximize()
             } else {
                 client = webdriverio
                     .remote(options)
                     .init()
-                    .windowHandleMaximize()
             }
             initCommands(client);
 
--- test/itg/1.7/common.webdriverio.js
+++ test/itg/1.7/common.webdriverio.js
@@ -98,12 +98,10 @@ module.exports = {
                 client = webdriverio
                     .remote(options2)
                     .init()
-                    .windowHandleMaximize()
             } else {
                 client = webdriverio
                     .remote(options)
                     .init()
-                    .windowHandleMaximize()
             }
             initCommands(client);
 
