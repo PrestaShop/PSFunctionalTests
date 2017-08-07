5d14c3b6076710e6cd3667f616984a33a4dd2f58
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-07 17:55:45 +0100

    Ui mapping  1.7

--- test/itg/1.6/common.webdriverio.js
+++ test/itg/1.6/common.webdriverio.js
@@ -118,6 +118,7 @@ module.exports = {
                 client = webdriverio
                     .remote(options2)
                     .init()
+
             } else {
                 client = webdriverio
                     .remote(options)
