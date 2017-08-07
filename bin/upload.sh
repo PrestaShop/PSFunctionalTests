5457d995599863b7f383e0265f1367a777430480
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-07 12:05:22 +0100

    Adding the Gdrive mocha reporter parameter

--- bin/upload.sh
+++ bin/upload.sh
@@ -4,6 +4,19 @@ FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
 ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.tar.bz2
 DIR="test/itg/$PS_VERSION/screenshots"
 
+
+echo "Creating archive $ARCHIVE_REPORTER"
+
+ARCHIVE_REPORTER=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.zip
+
+echo "Creating archive $ARCHIVE_REPORTER"
+
+zip -r $ARCHIVE_REPORTER test/itg/$PS_VERSION/mochawesome-report/*
+
+
+./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE_REPORTER"
+
+
 # look for empty dir
 if [ "$(ls -A $DIR)" ]; then
     echo "Creating archive $ARCHIVE"
