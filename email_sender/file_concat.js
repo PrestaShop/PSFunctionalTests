4fc0e1a44c421edf6bbc1936c93569f3be5d3d59
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-09 11:35:49 +0100

    Adding verification of file existance

--- email_sender/file_concat.js
+++ email_sender/file_concat.js
@@ -6,43 +6,56 @@ var i =0 ;
 var Version = new Array();
 Version = [1.6,1.7];
 
+
+
+
+
+
 while (i < 2) {
-    var cssfile = '';
-    var jsfile = '';
-    var htmlfile= '';
 
-    console.log(Version[i])
 
-    // concatenate css and js files in a single html file
-    function  processFile () {
-        jsfile="<script type='text/javascript'>"+jsfile+"</script></body>";
-        cssfile="<style type='text/css'>"+cssfile+"</style>";
+    if (fs.existsSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report')) {
+        var cssfile = '';
+        var jsfile = '';
+        var htmlfile= '';
 
-        htmlfile = htmlfile.replace('<link rel="stylesheet" href="assets/app.css"/>', '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+cssfile);
-        htmlfile = htmlfile.replace('</body>', jsfile);
-        fs.writeFile("email_sender/rapport_test_"+Version[i]+".html", htmlfile, function(err) {
+        console.log(Version[i])
 
-            if(err) {
-                return console.log(err);
-            }
+        // concatenate css and js files in a single html file
+        function  processFile () {
+            jsfile="<script type='text/javascript'>"+jsfile+"</script></body>";
+            cssfile="<style type='text/css'>"+cssfile+"</style>";
 
-            console.log("The file was saved!");
-        });
+            htmlfile = htmlfile.replace('<link rel="stylesheet" href="assets/app.css"/>', '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+cssfile);
+            htmlfile = htmlfile.replace('</body>', jsfile);
+            fs.writeFile("email_sender/rapport_test_"+Version[i]+".html", htmlfile, function(err) {
 
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            });
+
+        }
+
+
+        // read content of Js file
+        jsfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/assets/app.js').toString();
+
+        // read content of CSS file
+        cssfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/assets/app.css').toString();
+
+        // read content of Html file
+        htmlfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/mochawesome.html').toString();
+
+        processFile();
+
+        i++;
+    }else{
+        i++
     }
 
-    // read content of Js file
-    jsfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/assets/app.js').toString();
-
-    // read content of CSS file
-    cssfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/assets/app.css').toString();
-
-    // read content of Html file
-    htmlfile = fs.readFileSync('/home/travis/build/fouratachour/PSFunctionalTests/test/itg/'+Version[i]+'/mochawesome-report/mochawesome.html').toString();
-
-    processFile();
-
-    i++;
 }
 
 
